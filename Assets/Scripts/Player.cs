ac8a3f5dd05df0a559403f3b6ec3b93d4824fc4a
KorneyKSam <65783294+KorneyKSam@users.noreply.github.com>
2022-02-27 12:23:21 +0700

    V02 Correct Lerp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,40 @@
-using UnityEngine;
 using EventSystem;
+using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private AnimationCurve _curve;
-    [SerializeField] private float _speed = 0.5f;
     [SerializeField] private Transform _playerTransform;
-    private Vector3 _newPosition;
+    [SerializeField] private float _laneOffset = 6f;
+    [SerializeField] private AnimationCurve _curve;
+    [SerializeField] private float _lerpDuration = 0.5f;
 
-    private Vector3 leftPosition = new Vector3(-6, 0, 0);
-    private Vector3 rightPosition = new Vector3(6, 0, 0);
+    private Vector3 _targetPosition;
+    private float _timeElapsed;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _targetPosition = transform.position;
+
+        _rigidbody = GetComponentInChildren<Rigidbody>();
+        if (_rigidbody == null)
+        {
+            throw new NullReferenceException("Player child doesn't have a Rigidbody!");
+        }
+    }
 
     private void FixedUpdate()
     {
-        transform.localPosition = Vector3.Lerp(transform.localPosition, _newPosition, Time.deltaTime);
+        if (_timeElapsed < _lerpDuration)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _targetPosition, _curve.Evaluate(_timeElapsed / _lerpDuration));
+            _timeElapsed += Time.deltaTime;
+        }
+        else
+        {
+            transform.localPosition = _targetPosition;
+        }
     }
 
     private void OnEnable()
@@ -30,13 +51,21 @@ public class Player : MonoBehaviour
 
     private void TurnToLeft()
     {
-        _newPosition = leftPosition;
         Debug.Log("To Left");
+        if (_targetPosition.x > -_laneOffset)
+        {
+            _timeElapsed = 0;
+            _targetPosition = new Vector3(_targetPosition.x - _laneOffset, transform.position.y, transform.position.z);
+        }
     }
 
     private void TurnToRight()
     {
-        _newPosition = rightPosition;
         Debug.Log("To Right");
+        if (_targetPosition.x < _laneOffset)
+        {
+            _timeElapsed = 0;
+            _targetPosition = new Vector3(_targetPosition.x + _laneOffset, transform.position.y, transform.position.z);
+        }
     }
 }
